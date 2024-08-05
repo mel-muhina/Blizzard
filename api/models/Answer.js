21f5b6cf5b0aa3e8f6c1d51531820e8ff82f1752
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-05 19:46:31 +0100

    Complete question route

---
+++ api/models/Answer.js
@@ -0,0 +1,9 @@
+class Answer {
+  constructor({ answer_id, answers, question_id }) {
+    this.answer_id = answer_id;
+    this.answers = answers;
+    this.question_id = question_id;
+  }
+}
+
+module.exports = Answer;
