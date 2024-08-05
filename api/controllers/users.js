92df79f2091546fe8bb9413158db8591df3b11eb
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-05 15:29:57 +0100

    Create authentication

---
+++ api/controllers/users.js
@@ -0,0 +1,67 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+const login = async (req, res) => {
+  try {
+    const data = req.body;
+
+    const user = await User.findByUsername(data.username);
+
+    const match = await bcrypt.compare(data.password, user.password);
+
+    if (!match) throw new Error("Unable to authenticate");
+
+    jwt.sign(
+      { user_id: user.user_id },
+      process.env.SECRET_TOKEN,
+      {
+        expiresIn: 3600,
+      },
+      (err, data) => {
+        if (err) {
+          res
+            .status(500)
+            .json({ success: false, error: "Error generating token" });
+        } else {
+          res.status(200).json({ success: true, data: { token: data } });
+        }
+      }
+    );
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+
+const signup = async (req, res) => {
+  const data = req.body;
+  try {
+    const salt = await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS));
+
+    data.password = await bcrypt.hash(data.password, salt);
+
+    const newUser = await User.create(data);
+
+    jwt.sign(
+      { user_id: newUser.user_id },
+      process.env.SECRET_TOKEN,
+      {
+        expiresIn: 3600,
+      },
+      (err, data) => {
+        if (err) {
+          res
+            .status(500)
+            .json({ success: false, error: "Error generating token" });
+        } else {
+          res.status(201).json({ success: true, data: { token: data } });
+        }
+      }
+    );
+  } catch (err) {
+    res.status(404).send({ success: false, error: err });
+  }
+};
+
+module.exports = { login, signup };
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 
+const usersControllers = require("../controllers/users");
+
 const usersRouter = Router();
 
-usersRouter.get("/signup");
-usersRouter.post("/login");
+usersRouter.get("/signup", usersControllers.signup);
+usersRouter.post("/login", usersControllers.login);
 
 module.exports = usersRouter;
