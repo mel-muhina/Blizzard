4624b0381e69faa1586ed957565c6b519e004614
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-05 16:29:20 +0100

    Add character controller

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const questionsRouter = require("./routes/questions");
 const usersRouter = require("./routes/users");
+const eventsRouter = require("./routes/events");
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/questions", questionsRouter);
+app.use("/events", eventsRouter);
 app.use("/users", usersRouter);
 
 module.exports = app;
---
+++ api/models/Question.js
@@ -0,0 +1,5 @@
+const db = require("../database/connect");
+
+class Question {}
+
+module.exports = Question;
