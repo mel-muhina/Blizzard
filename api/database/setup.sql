bb34e172666524b212692db67adf0fd8856fea6f
Alena Pavlioglo <alena.pavlioglo@lafosseacademy.com>
2024-08-05 16:01:26 +0100

    Insert info into tables

--- api/database/setup.sql
+++ api/database/setup.sql
@@ -54,3 +54,30 @@ CREATE TABLE Characters (
   character_name VARCHAR(50),
   birth_year INT
 );
+
+
+INSERT INTO Characters (character_id, character_name, birth_year) 
+VALUES (1, 'Julius Caesar', -100); 
+
+
+INSERT INTO Events (events_id, character_id, event_date, event_description) 
+VALUES (1, 1, '60 BCE', 'In 60 BCE, Rome conflicts with itself as various leaders seek control. Julius Caesar considers forming an alliance with Pompey the Great and Crassus to boost his power.');
+
+
+INSERT INTO Questions (question_id, Question_description, answer_id, event_id, score) 
+VALUES (1, 'It is 60 BCE, Rome conflicts with itself, various leaders looking to seize control for themselves. Caesar comes up with a plan to form an alliance with another leader to boost his own power and control over the empire. Some leaders Caesar considers are Pompey the Great and Crassus. It is your job to advise him on the best course of action: a) Side with Pompey, b) Form an alliance with Crassus, c) Take the chance and form an alliance with both men.', NULL, 1, 10);
+
+
+INSERT INTO Answers (answer_id, answers, question_id) 
+VALUES 
+  (1, 'Side with Pompey, that way he gains further military power through his help.', 1),
+  (2, 'Form an alliance with Crassus to gain further wealth and influence over the land, allowing him to garner further support later.', 1),
+  (3, 'Take the chance in forming an alliance with both men which could be risky.', 1);
+
+
+INSERT INTO Answers (answer_id, answers, question_id) 
+VALUES (4, 'Julius Caesar formed a Triumvirate where he was able to garner both military support and wealth through both men thus allowing him more power, dominance, and influence over Rome, eventually securing the governorship over Gaul and advancing his military career.', 1);
+
+
+INSERT INTO Submissions (user_id, question_id, outcome) 
+VALUES (1, 1, TRUE); 
