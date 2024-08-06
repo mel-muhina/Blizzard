c3e0dfcf2c3edc7e83194ed638d567c4f0972cb1
Mel <160260061+mel-muhina@users.noreply.github.com>
2024-08-06 12:16:42 +0100

    Add checkforanswers and checkforquestions in game logic.

--- client/assets/gameLogic/logic.js
+++ client/assets/gameLogic/logic.js
@@ -5,16 +5,37 @@ class gameState {
     this.character = character;
     this.lives = lives;
     this.event = event;
+    this.eventIndex = 0;
   }
 
-  static async fetchForUser() {
+  //   static async fetchForUser() {
+  //     try {
+  //       const response = await fetch(
+  //         `https://blizzard-5jur.onrender.com/characters/1`
+  //       );
+
+  //       if (response.ok) {
+  //         const data = await response.json();
+  //         console.log("check data", data);
+  //         data.forEach((character) => addEntry(entry)); // Map over all entries
+  //       } else {
+  //         throw new Error("Error: " + response.status);
+  //       }
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   }
+
+  async fetchForCharacter(id) {
     try {
-      const response = await fetch(`https://blizzard-5jur.onrender.com`);
+      const response = await fetch(
+        `https://blizzard-5jur.onrender.com/characters/${id}`
+      );
 
       if (response.ok) {
-        console.log("check response", response);
         const data = await response.json();
-        data.forEach((entry) => addEntry(entry)); // Map over all entries
+        this.character = data;
+        console.log("Check fetchForCharacter", this.character);
       } else {
         throw new Error("Error: " + response.status);
       }
@@ -23,16 +44,69 @@ class gameState {
     }
   }
 
-  static async fetchForCharacter() {}
+  //   static async fetchForEvents() {
+  //     try {
+  //       const response = await fetch(
+  //         `https://blizzard-5jur.onrender.com/characters/${id}`
+  //       );
 
-  static async fetchForEvents() {}
+  //       if (response.ok) {
+  //         const data = await response.json();
+  //         this.character = data;
+  //         // console.log("check character", this.character);
+  //       } else {
+  //         throw new Error("Error: " + response.status);
+  //       }
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   }
 
-  static async fetchForQuestions() {}
+  async fetchForQuestions(id) {
+    try {
+      const response = await fetch(
+        `https://blizzard-5jur.onrender.com/questions/${id}`
+      );
 
-  static async checkForAnswers() {}
+      if (response.ok) {
+        const data = await response.json();
+        this.question = data;
+        console.log("Check fetchforQuestions", this.question);
+      } else {
+        throw new Error("Error: " + response.status);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async checkForAnswers(id) {
+    console.log("checkForAnswers question", this.question);
+
+    console.log("checkForAnswers", this.question.answer_id);
+    if (this.question.answer_id === id) {
+      console.log("id check", id);
+      this.eventIndex += 1;
+      console.log("checkForAnswers index", this.eventIndex);
+    } else {
+      console.log("check for ans fail", this.lives);
+      this.lives -= 1;
+    }
+  }
 }
 
-const test = new gameState(fetchForUser);
+const test = new gameState({
+  user_highscore: 0,
+  question: {},
+  event: {},
+  lives: 3,
+  character: {},
+  eventIndex: 1,
+});
+
+test.fetchForQuestions(1);
+test.fetchForCharacter(1);
+test.checkForAnswers(1);
 
 module.exports = {
   test,
