4824764ca20fbcc01b9a84baab494c400233a1b2
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-06 11:12:30 +0100

    Add watchify

--- client/assets/dist/gameBundle.js
+++ client/assets/dist/gameBundle.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const GameState = require("./logic.js");
 const quizOptions = document.querySelectorAll("#table .option");
 const questionDescription = document.querySelector(".question-description");
@@ -24,3 +25,11 @@ const updateQuestion = () => {
     quizOptions[i].textContent = answer.answers;
   });
 };
+
+},{"./logic.js":2}],2:[function(require,module,exports){
+
+
+
+
+
+},{}]},{},[1]);
--- client/script.js
+++ client/script.js
@@ -1,4 +1,7 @@
+const Model = require("./logic.js");
 const quizOptions = document.querySelectorAll("#table .option");
+const questionDescription = document.querySelector(".question-description");
+
 console.log("quizOptions test", quizOptions[0]);
 
 quizOptions[0].addEventListener("click", () => {
@@ -13,3 +16,8 @@ quizOptions[2].addEventListener("click", () => {
   console.log("This is option 3");
 });
 // https://blizzard-5jur.onrender.com/questions/1
+
+const updateQuestion = () => {
+  const question = Model.fetchForQuestion();
+  questionDescription.textContent = gameState.question.questionDescription;
+};
