42c670c347b63108d6d0314211e7392a4d1c1e60
Alos444 <alenilusa@gmail.com>
2024-08-06 01:20:19 +0100

    Remove changes

--- api/database/setup.js
+++ api/database/setup.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const db = require('./connect');  // Adjust this path if necessary
+require("dotenv").config()
+const fs = require("fs")
 
+const db = require("../database/connect")
 const sql = fs.readFileSync(__dirname + '/setup.sql').toString();
 
 db.query(sql)
-  .then(() => {
-    db.end();
-    console.log('Setup complete');
-  })
-  .catch(error => console.error('Error executing SQL:', error));
-
+.then(data => {
+    db.end()
+    console.log("Setup complete")
+})
+.catch(error => console.log(error))
