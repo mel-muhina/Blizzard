c86409262c5c266eb4f963a9e249f512c39d4d61
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-07 16:17:58 +0100

    Add order by to show in Event model

--- api/models/Event.js
+++ api/models/Event.js
@@ -1,19 +1,23 @@
 const db = require("../database/connect");
 
 class Event {
-  constructor({ event_id, character_id, event_date, event_description, bg_image_url, char_image_url }) {
+  constructor({
+    event_id,
+    character_id,
+    event_date,
+    bg_image_url,
+    char_image_url,
+  }) {
     this.event_id = event_id;
     this.character_id = character_id;
     this.event_date = event_date;
-    this.event_description = event_description;
     this.bg_image_url = bg_image_url;
     this.char_image_url = char_image_url;
-
   }
 
   static async show(character_id) {
     const events = await db.query(
-      "SELECT * FROM events WHERE character_id = $1",
+      "SELECT * FROM events WHERE character_id = $1 ORDER BY event_id",
       [character_id]
     );
 
