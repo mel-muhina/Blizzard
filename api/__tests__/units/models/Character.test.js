c172cfdbe73035800b720e74e01b8492c1bcca72
mel-muhina <mel.muhina@gmail.com>
2024-08-07 19:47:18 +0100

    Add character model tests.

--- api/__tests__/units/models/Character.test.js
+++ api/__tests__/units/models/Character.test.js
@@ -1,97 +1,71 @@
 const db = require("../../../database/connect");
-const Answer = require("../../../models/Answer");
-const Question = require("../../../models/Question");
+const Character = require("../../../models/Character");
+// const Answer = require("../../../models/Answer");
+// const Question = require("../../../models/Question");
 
-describe("Question model", () => {
-  describe("showByEvent", () => {
+describe("Character model", () => {
+  describe("getOneById", () => {
     beforeEach(() => {
       jest.clearAllMocks();
     });
 
-    it("Returns an instance of a question", async () => {
-      const mockQuestion = {
-        question_id: 1,
-        question_description: "Mock question",
-        answer_id: 1,
-        event_id: 1,
-        score: 10,
-      };
+    it("Fetchs character by id", async () => {
+        const mockCharacter = {
+            character_id: 1,
+            character_name: "Julius Caesar",
+            birth_year: -100,
+            image_url: "https://simplycharly.com/wp-content/uploads/2022/07/caesar-scaled.jpeg"
+          };
       const mockId = 1;
 
-      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [mockQuestion] });
+      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [mockCharacter] });
 
-      const result = await Question.showByEvent(mockId);
+      const result = await Character.getOneById(mockId);
 
-      expect(result).toBeInstanceOf(Question);
-      expect(db.query).toBeCalledWith(
-        "SELECT * FROM question WHERE event_id = $1",
-        [mockId]
-      );
+      expect(result).toBeInstanceOf(Character);
+      expect(db.query).toBeCalledWith("SELECT * FROM characters WHERE character_id = $1;",[mockId]);
     });
 
-    it("Throws an error when no question is found", async () => {
+    it("Throws an error when no character is found", async () => {
       const mockId = 1;
 
       jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [] });
 
-      await expect(Question.showByEvent(mockId)).rejects.toThrow(
-        "Error finding question"
-      );
+      await expect(Character.getOneById(mockId)).rejects.toThrow("Unable to locate chosen character.");
 
-      expect(db.query).toBeCalledWith(
-        "SELECT * FROM question WHERE event_id = $1",
-        [mockId]
-      );
+      expect(db.query).toBeCalledWith("SELECT * FROM characters WHERE character_id = $1;",[mockId]);
     });
   });
 
-  describe("getAnswers", () => {
+  describe("getAll", () => {
     beforeEach(() => {
       jest.clearAllMocks();
     });
 
-    it("Returns an array of Answer instances", async () => {
-      const mockQuestion = {
-        question_id: 1,
-        question_description: "Mock question",
-        answer_id: 1,
-        event_id: 1,
-        score: 10,
-      };
-      const question = new Question(mockQuestion);
+    it("resolves with characters on successful db query", async () => {
+        const mockCharacter = [{
+            character_id: 1,
+            character_name: "Julius Caesar",
+            birth_year: -100,
+            image_url: "https://simplycharly.com/wp-content/uploads/2022/07/caesar-scaled.jpeg"
+          }];
+     
+         jest.spyOn(db, "query").mockResolvedValueOnce({ rows: mockCharacter });
 
-      const mockAnswers = [
-        { answer_id: 1, answers: "mock1", question_id: 1 },
-        { answer_id: 2, answers: "mock2", question_id: 1 },
-        { answer_id: 3, answers: "mock3", question_id: 1 },
-      ];
+      const characters = await Character.getAll();
 
-      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: mockAnswers });
-
-      const answers = await question.getAnswers();
-
-      expect(answers[0]).toBeInstanceOf(Answer);
+      expect(characters[0]).toBeInstanceOf(Character);
       expect(db.query).toHaveBeenCalledTimes(1);
       expect(db.query).toHaveBeenCalledWith(
-        "SELECT answers , answer_id FROM answers WHERE question_id = $1",
-        [mockQuestion.question_id]
+        "SELECT * FROM characters;"
       );
     });
 
-    it("Throws an Error if no Answers are found", async () => {
-      const mockQuestion = {
-        question_id: 1,
-        question_description: "Mock question",
-        answer_id: 1,
-        event_id: 1,
-        score: 10,
-      };
-      const question = new Question(mockQuestion);
-
+    it("Throws an Error if no characters are found", async () => {
       jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [] });
 
-      await expect(question.getAnswers()).rejects.toThrow(
-        "Error finding answers"
+      await expect(Character.getAll()).rejects.toThrow(
+        "Unable to locate any characters."
       );
     });
   });
