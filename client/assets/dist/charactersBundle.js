a9fceb36a0d7a9f21e665e8e68fda44c0107ccf5
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-07 17:53:25 +0100

    Refactored characters page

--- client/assets/dist/charactersBundle.js
+++ client/assets/dist/charactersBundle.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const checkAuth = require("../utils/checkAuth");
 
 const cardsContainer = document.getElementById("cardsContainer");
@@ -49,3 +50,25 @@ function renderCards(characters) {
   const characters = await fetchCharacterData();
   renderCards(characters);
 })();
+
+},{"../utils/checkAuth":2}],2:[function(require,module,exports){
+async function checkAuth() {
+  const options = {
+    method: "GET",
+    headers: {
+      authorization: localStorage.getItem("token"),
+    },
+  };
+  const response = await fetch(
+    "https://blizzard-5jur.onrender.com/users/validate-token",
+    options
+  );
+
+  if (response.status !== 200) {
+    window.location.href = "login.html";
+  }
+}
+
+module.exports = checkAuth;
+
+},{}]},{},[1]);
