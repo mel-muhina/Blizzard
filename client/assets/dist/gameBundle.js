c4488395b904614707cc41893d5c604ad8f81ee7
mel-muhina <mel.muhina@gmail.com>
2024-08-07 19:51:33 +0100

    Resolve gamebundle.

--- client/assets/dist/gameBundle.js
+++ client/assets/dist/gameBundle.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-console.error("Error: Can't walk dependency graph: Cannot find module 'bootstrap' from 'C:\\Users\\sweet\\Desktop\\Projects\\Blizzard\\Blizzard\\client\\assets\\gameLogic\\view\\viewWin.js'\n    required by C:\\Users\\sweet\\Desktop\\Projects\\Blizzard\\Blizzard\\client\\assets\\gameLogic\\view\\viewWin.js");
-=======
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const GameState = require("./logic.js");
 const winModal = require("./view/viewWin.js");
@@ -6604,4 +6601,3 @@ exports.preventOverflow = preventOverflow$1;
 
 
 },{"@popperjs/core":6}]},{},[1]);
->>>>>>> dev
